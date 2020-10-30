ebf3ee76da9f6d92771b9942acafd0b648ca76c3
Danilo Cunha <danilowlc@gmail.com>
2020-10-29 22:31:06 -0300

    first commit

---
+++ GoStack/bootcamp/nivel1/frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header title="HomePage">
+        <ul>
+          <li>HomePage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>HomePage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
+    </>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,4 @@
+###Rocketseat GoStack
+
+- Repositorio criado para guardar os codigos produzidos durante o bootcamp GoStack
+
