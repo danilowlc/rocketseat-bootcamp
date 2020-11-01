358aa66bdea25e3a382b386f0ee6b911ac18c756
Danilo Cunha <danilowlc@gmail.com>
2020-11-01 17:54:53 -0300

    Aula Conceitos React-Native

---
+++ GoStack/bootcamp/nivel1/mobile/src/index.js
@@ -0,0 +1,94 @@
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+import api from './services/api';
+
+export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then((response) => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+      const response = await api.post('projects', {
+          title: `New Project ${Date.now()}`,
+          owner: 'Danilo Mobile'
+      })
+      const project = response.data
+      setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="purple" />
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.title}> Project List </Text>
+        </View>
+        <FlatList
+          style={styles.list}
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({item: project}) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.button}
+          onPress={handleAddProject}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  project: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 3,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  list: {
+    backgroundColor: '#7159c1',
+    padding: 8,
+  },
+  button: {
+    backgroundColor: '#fff',
+    alignSelf: 'stretch',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
---
+++ GoStack/bootcamp/nivel1/mobile/src/services/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3333/',
+});
+
+export default api;
+
+/**
+ * iOS com emulador: localhost
+ * iOS Fisico: IP da maquina
+ * Android com emulador: adb reverse tcp:3333 tcp:3333 para usar localhost
+ * Android com emulador: 10.0.2.2 (Android Studio)
+ * Android com emulador: 10.0.3.2 (Genymotiom)
+ * Android Fisico: IP da maquina
+ */
