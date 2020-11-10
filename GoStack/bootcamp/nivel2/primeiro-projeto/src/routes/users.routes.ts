b78ee8f15997c51d5c6ea49672d95c29fe0547a8
Danilo Cunha <danilowlc@gmail.com>
2020-11-09 21:43:58 -0300

    correcao do retorno da password

--- GoStack/bootcamp/nivel2/primeiro-projeto/src/routes/users.routes.ts
+++ GoStack/bootcamp/nivel2/primeiro-projeto/src/routes/users.routes.ts
@@ -18,9 +18,15 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    delete user.password;
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    }
 
-    return response.json(user);
+    return response.json(userWithoutPassword);
   } catch (error) {
     return response.status(400).json({ error: error.message });
   }
