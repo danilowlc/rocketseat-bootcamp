0be7a76aaf17ac3b7a579713ece5351d37021f2d
Danilo Cunha <danilowlc@gmail.com>
2020-11-24 22:40:57 -0300

    implementando autenticaçao do usuario

---
+++ GoStack/bootcamp/nivel3/gobarber-web/src/hooks/AuthContext.tsx
@@ -0,0 +1,73 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/apiClient';
+
+interface AuthState {
+  token: string;
+  userWithoutPassword: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  userWithoutPassword: object;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const userWithoutPassword = localStorage.getItem(
+      '@GoBarber:userWithoutPassword ',
+    );
+    if (token && userWithoutPassword) {
+      return { token, userWithoutPassword: JSON.parse(userWithoutPassword) };
+    }
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    console.log(response.data);
+    const { token, userWithoutPassword } = response.data;
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem(
+      '@GoBarber:userWithoutPassword',
+      JSON.stringify(userWithoutPassword),
+    );
+
+    setData({ token, userWithoutPassword });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:userWithoutPassword');
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ userWithoutPassword: data.userWithoutPassword, signIn, signOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+  return context;
+}
--- GoStack/bootcamp/nivel3/gobarber-web/src/pages/SignIn/index.tsx
+++ GoStack/bootcamp/nivel3/gobarber-web/src/pages/SignIn/index.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import { useAuth } from '../../hooks/AuthContext';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,25 +14,69 @@ import Button from '../../components/Button';
 
 import { Background, Container, Content } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber Logomarca" />
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
-      <form>
-        <h1>Faça seu logon</h1>
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-        <Button type="submit">Entrar</Button>
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
-      <a href="forgot">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { userWithoutPassword, signIn } = useAuth();
+  console.log(userWithoutPassword);
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const WARNREQUIRED = 'Campo Obrigatório';
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required(WARNREQUIRED)
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required(WARNREQUIRED),
+        });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (error) {
+        const errors = getValidationErrors(error);
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [signIn],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber Logomarca" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+        <a href="forgot">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
