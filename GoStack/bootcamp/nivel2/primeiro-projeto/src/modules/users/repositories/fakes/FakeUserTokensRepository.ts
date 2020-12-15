1f7ea115432164e0866df92f025c3d383276c248
Danilo Cunha <danilowlc@gmail.com>
2020-12-14 22:00:28 -0300

    criacao dos testes de recupercao de senha

--- GoStack/bootcamp/nivel2/primeiro-projeto/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ GoStack/bootcamp/nivel2/primeiro-projeto/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -5,8 +5,13 @@ import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
 
 
 class FakeUserTokensRepository implements IUserTokensRepository{
-
   private userTokens: UserToken[] = [];
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(findToken => findToken.token === token);
+    return userToken;
+  }
+
 
   public async generate(user_id: string): Promise<UserToken> {
     const userToken = new UserToken();
@@ -14,13 +19,16 @@ class FakeUserTokensRepository implements IUserTokensRepository{
     Object.assign(userToken, {
       id: uuid(),
       token: uuid(),
-      user_id
+      user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     })
 
     this.userTokens.push(userToken);
 
     return userToken;
   }
+
 
 }
 
