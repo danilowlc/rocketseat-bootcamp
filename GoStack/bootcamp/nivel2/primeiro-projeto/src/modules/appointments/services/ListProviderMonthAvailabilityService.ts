f41bd9998b50255120be0ca424510f5274f1ac2f
Danilo Cunha <danilowlc@gmail.com>
2020-12-21 20:29:50 -0300

    list providers

--- GoStack/bootcamp/nivel2/primeiro-projeto/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ GoStack/bootcamp/nivel2/primeiro-projeto/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -29,13 +30,26 @@ class ListProviderMonthAvailabilityService {
       year
     },);
 
-    console.log(appointments);
+    const numberDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [
-      {
-        day: 1, available: false
-      }
-    ];
+    const eachDayArray = Array.from(
+      {length: numberDaysInMonth},
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+
+    });
+
+    return availability;
   }
 }
 export default ListProviderMonthAvailabilityService;
